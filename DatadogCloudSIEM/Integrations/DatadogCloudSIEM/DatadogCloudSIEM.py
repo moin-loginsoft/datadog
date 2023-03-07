94f6762b9894c6df12db015d296a061f69913480
moin-loginsoft <mmoinuddin@loginsoft.com>
2023-03-07 15:00:28 +0530

    events module

--- DatadogCloudSIEM/Integrations/DatadogCloudSIEM/DatadogCloudSIEM.py
+++ DatadogCloudSIEM/Integrations/DatadogCloudSIEM/DatadogCloudSIEM.py
@@ -20,8 +20,7 @@ from CommonServerUserPython import *  # noqa
 
 import urllib3
 from typing import Dict, Any
-
-from os import environ
+import dateparser
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v1.api.authentication_api import AuthenticationApi
 from datadog_api_client.v1.api.events_api import EventsApi
@@ -45,15 +44,13 @@ PAGE_NUMBER_ERROR_MSG = "Invalid Input Error: page number should be greater than
 PAGE_SIZE_ERROR_MSG = "Invalid Input Error: page size should be greater than zero."
 DEFAULT_FROM_DATE = "-7days"
 DEFAULT_TO_DATE = "now"
-INTEGRATION_CONTEXT_NAME = 'Datadog'
+INTEGRATION_CONTEXT_NAME = "Datadog"
 
 """ HELPER FUNCTIONS """
 
 
 def date_string_to_timestamp(date: str) -> int:
-    return int(
-        datetime.strptime(date, DATE_FORMAT).timestamp()
-    )
+    return int(datetime.strptime(date, DATE_FORMAT).timestamp())
 
 
 def str_to_bool(s):
@@ -62,16 +59,17 @@ def str_to_bool(s):
     'True', 'true', 'Yes', 'yes', and '1' are considered True.
     'False', 'false', 'No', 'no', and '0' are considered False.
     """
-    if s.lower() in ('true', 'yes', '1'):
+    if s.lower() in ("true", "yes", "1"):
         return True
-    elif s.lower() in ('false', 'no', '0'):
+    elif s.lower() in ("false", "no", "0"):
         return False
     else:
         raise ValueError("Invalid string value for boolean conversion: " + s)
 
 
-def get_command_title_string(sub_context: str, page: Optional[int],
-                             page_size: Optional[int]) -> str:
+def get_command_title_string(
+    sub_context: str, page: Optional[int], page_size: Optional[int]
+) -> str:
     """
     Define command title
     Args:
@@ -82,8 +80,10 @@ def get_command_title_string(sub_context: str, page: Optional[int],
         Returns the title for the readable output
     """
     if page and page_size and (page > 0 and page_size > 0):
-        return f'{sub_context} List\nCurrent page size: {page_size}\n' \
-               f'Showing page {page} out of others that may exist'
+        return (
+            f"{sub_context} List\nCurrent page size: {page_size}\n"
+            f"Showing page {page} out of others that may exist"
+        )
 
     return f"{sub_context} List"
 
@@ -114,23 +114,24 @@ def lookup_to_markdown(results: List[Dict], title: str) -> str:
     markdown = tableToMarkdown(title, results, headers=headers, removeNull=True)
     return markdown
 
+
 def event_for_lookup(event: Dict) -> Dict:
     event_dict = {
         "Title": event.get("title"),
         "Text": event.get("text"),
-        "Date Happened": datetime.fromtimestamp(
-            event.get("date_happened", 0)
-        ).strftime("%Y-%m-%d %H:%M:%S"),
+        "Date Happened": datetime.fromtimestamp(event.get("date_happened", 0)).strftime(
+            "%Y-%m-%d %H:%M:%S"
+        ),
         "Id": event.get("id"),
         "Priority": event.get("priority"),
         "Source": event.get("source"),
         "Tags": ",".join(tag for tag in event.get("tags", [])),
-        "Is Aggregate": event.get('is_aggregate'),
-        'Host': event.get('host'),
-        'Device Name': event.get('device_name'),
-        'Alert Type': event.get('alert_type'),
+        "Is Aggregate": event.get("is_aggregate"),
+        "Host": event.get("host"),
+        "Device Name": event.get("device_name"),
+        "Alert Type": event.get("alert_type"),
     }
-    return  event_dict
+    return event_dict
 
 
 def pagination(limit: Optional[int], page: Optional[int], page_size: Optional[int]):
@@ -166,8 +167,6 @@ def pagination(limit: Optional[int], page: Optional[int], page_size: Optional[in
     return limit, offset
 
 
-# TODO: ADD HERE ANY HELPER FUNCTION YOU MIGHT NEED (if any)
-
 """ COMMAND FUNCTIONS """
 
 
@@ -177,9 +176,6 @@ def test_module(configuration) -> str:
     Returning 'ok' indicates that the integration works like it is supposed to.
     Connection to the service is successful.
     Raises exceptions if something goes wrong.
-
-    :type client: ``Client``
-    :param Client: client to use
 
     :return: 'ok' if test passed, anything else will fail the test.
     :rtype: ``str``
@@ -196,10 +192,8 @@ def test_module(configuration) -> str:
     return message
 
 
-
-
 def create_event_command(configuration, args):
-    priority = args.get('priority')
+    priority = args.get("priority")
     alert_type = args.get("alert_type")
     if priority and priority not in EventPriority.allowed_values:
         return DemistoException("Priority not in allowed values.")
@@ -207,9 +201,13 @@ def create_event_command(configuration, args):
         return DemistoException("Alert type not in allowed values.")
     date_happened = args.get("date_happened")
     if date_happened:
-        date_happened_timestamp = date_string_to_timestamp(date_happened)
+        date_happened_timestamp = int(
+            dateparser.parse(date_happened, settings={"TIMEZONE": "UTC"}).timestamp()
+        )
         if not is_within_18_hours(date_happened_timestamp):
-            readable_output = "The time of the event shall not be older than 18 hours!\n"
+            readable_output = (
+                "The time of the event shall not be older than 18 hours!\n"
+            )
             return CommandResults(readable_output=readable_output)
 
     data = {
@@ -217,12 +215,18 @@ def create_event_command(configuration, args):
         "text": args.get("text"),
         "tags": args.get("tags").split(",") if args.get("tags") else None,
         "alert_type": EventAlertType(args.get("alert_type")),
-        "priority": EventPriority(args.get('priority')),
+        "priority": EventPriority(args.get("priority")),
         "aggregation_key": args.get("aggregation_key"),
-        "related_event_id": int(args.get("related_event_id")) if args.get("related_event_id") else None,
+        "related_event_id": int(args.get("related_event_id"))
+        if args.get("related_event_id")
+        else None,
         "host": args.get("host_name"),
         "device_name": args.get("device_name"),
-        "date_happened": date_string_to_timestamp(date_happened) if date_happened else None,
+        "date_happened": int(
+            dateparser.parse(date_happened, settings={"TIMEZONE": "UTC"}).timestamp()
+        )
+        if date_happened
+        else None,
         "source_type_name": args.get("source_type_name"),
     }
     data = {key: value for key, value in data.items() if value is not None}
@@ -258,8 +262,15 @@ def get_events_command(configuration, args):
                 readable_output = "No event to present.\n"
 
         else:
-            start_time = date_string_to_timestamp(args.get("start_date", (datetime.now() + timedelta(days=-7)).date().strftime(DATE_FORMAT)))
-            end_time = date_string_to_timestamp(args.get("end_date", datetime.now().strftime(DATE_FORMAT)))
+            start_time = int(
+                dateparser.parse(
+                    args.get("start_date", DEFAULT_FROM_DATE),
+                    settings={"TIMEZONE": "UTC"},
+                ).timestamp()
+            )
+            end_time = date_string_to_timestamp(
+                args.get("end_date", datetime.now().strftime(DATE_FORMAT))
+            )
             page = arg_to_number(args.get("page"), arg_name="page")
             page_size = arg_to_number(args.get("page_size"), arg_name="page_size")
             limit = arg_to_number(args.get("limit"), arg_name="limit")
@@ -271,15 +282,19 @@ def get_events_command(configuration, args):
                 "priority": args.get("priority"),
                 "sources": args.get("sources"),
                 "tags": args.get("tags"),
-                "unaggregated": str_to_bool(args.get("unaggregated")) if args.get("unaggregated") else None,
-                "exclude_aggregate": str_to_bool(args.get("exclude_aggregate")) if args.get("exclude_aggregate") else None,
+                "unaggregated": str_to_bool(args.get("unaggregated"))
+                if args.get("unaggregated")
+                else None,
+                "exclude_aggregate": str_to_bool(args.get("exclude_aggregate"))
+                if args.get("exclude_aggregate")
+                else None,
                 "page": datadog_page,
             }
 
             body = {key: value for key, value in body_dict.items() if value is not None}
             response = api_instance.list_events(**body)
             results = response.get("events", [])
-            resp = results[offset:offset + limit]
+            resp = results[offset : offset + limit]
             data = [event.to_dict() for event in resp]
             if data:
                 events_list = []
@@ -298,8 +313,6 @@ def get_events_command(configuration, args):
         )
 
 
-
-
 """ MAIN FUNCTION """
 
 
@@ -311,16 +324,17 @@ def main() -> None:
     demisto.debug(f"Command being called is {command}")
     try:
         configuration = Configuration()
-        configuration.api_key["apiKeyAuth"] = params.get('api_key') #"9a2e2f79385b2b996b45df7930bd8c6a"
-        configuration.api_key[
-            "appKeyAuth"] = params.get('app_key') #"2e28f1d651459c5bbb5bb26826cbbbc2a1d3de64"
-        # configuration.server_variables["site"] = params.get('site')
+        configuration.api_key["apiKeyAuth"] = params.get(
+            "api_key"
+        )
+        configuration.api_key["appKeyAuth"] = params.get(
+            "app_key"
+        )
+        configuration.server_variables["site"] = params.get('site')
 
         commands = {
-            "datadog-event-create":
-                create_event_command,
-            "datadog-event-list":
-                get_events_command,
+            "datadog-event-create": create_event_command,
+            "datadog-event-list": get_events_command,
         }
         if command == "test-module":
             return_results(test_module(configuration))
@@ -330,12 +344,10 @@ def main() -> None:
             raise NotImplementedError
         # Log exceptions
     except Exception as e:
-        return_error(
-            f'Failed to execute {command} command. Error: {str(e)}')
+        return_error(f"Failed to execute {command} command. Error: {str(e)}")
 
 
 """ ENTRY POINT """
 
 if __name__ in ("__main__", "__builtin__", "builtins"):
     main()
-
--- DatadogCloudSIEM/Integrations/DatadogCloudSIEM/DatadogCloudSIEM_test.py
+++ DatadogCloudSIEM/Integrations/DatadogCloudSIEM/DatadogCloudSIEM_test.py
@@ -12,7 +12,10 @@ you are implementing with your integration
 
 import json
 import io
-
+# import demistomock as demisto
+# import pytest
+# from DatadogCloudSIEM import create_event_command
+# from datadog_api_client import ApiClient, Configuration
 
 def util_load_json(path):
     with io.open(path, mode='r', encoding='utf-8') as f:
@@ -36,7 +39,63 @@ def test_baseintegration_dummy():
     }
     response = baseintegration_dummy_command(client, args)
 
-    mock_response = util_load_json('test_data/baseintegration-dummy.json')
+    mock_response = util_load_json('test_data/create-event-response.json')
 
     assert response.outputs == mock_response
 # TODO: ADD HERE unit tests for every command
+# CREATE_EVENT_RESPONSE = util_load_json("test_data/create-event-response.json")
+
+
+# def init_params():
+#     return {
+#         'app_key': 'WRONG_CLIENT_ID_TEST',
+#         'api_key': 'CLIENT_SECRET_TEST',
+#     }
+#
+
+# def test_test_module_raise_exception(mocker):
+#     mocker.patch.object(demisto, 'params', return_value=init_params())
+#     mocker.patch('requests.sessions.Session.send', return_value=MockedResponse(400))
+#
+#     from CybersixgillActionableAlerts import test_module
+#
+#     with pytest.raises(Exception):
+#         test_module()
+
+# @pytest.mark.parametrize('raw_response, expected', [(CREATE_EVENT_RESPONSE, CREATE_EVENT_RESPONSE)])
+# def test_create_event_command( raw_response, expected):
+#     config = Configuration()
+#     args ={
+#         "text": "A text",
+#         "title": "A title"
+#     }
+#     create_event_command(config, args)
+#     """
+#     Tests get_threat_list_command function.
+#
+#         Given:
+#             - mocker object.
+#             - raw_response test data.
+#             - expected output.
+#
+#         When:
+#             - Running the 'get_threat_list_command'.
+#
+#         Then:
+#             - Checks that the context output and the readable output of the command function are as expected.
+#     """
+#    mocker.patch.object(client, 'query_cisco_umbrella_api', side_effect=[raw_response] * 5)
+    # args = {
+    #     "limit": 5,
+    #     "from": 1662015255000,
+    #     "to": 1662447255000,
+    #     "offset": 0
+    # }
+    # with open(os.path.join('test_data', 'command_readable_output/threat_command_readable_output.md'), 'r') as f:
+    #     readable_output = f.read()
+    # command_results = get_threat_list_command(client, args)
+    #
+    # # results is CommandResults list
+    # context_detail = command_results.to_context()['Contents']
+    # assert context_detail == expected.get("data")
+    # assert command_results.readable_output == readable_output
